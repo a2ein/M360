fb1bcaa7fc892173b3f2bb4f29006a7c96425b70
a2ein <111061082+a2ein@users.noreply.github.com>
2023-06-14 20:59:37 +0600

    Upload to main

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 pannellum.viewer('panorama-360-view', {
     "type": "equirectangular",
     "panorama":"https://i.ibb.co/jDjTxk4/PANO-20230614-180837-0.jpg",
-    "autoLoad": true
+    "autoLoad": true,
+    "autoPlay": true
 })
