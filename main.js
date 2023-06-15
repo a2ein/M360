8073474949497df8776c404da0ba7302626d6921
a2ein <111061082+a2ein@users.noreply.github.com>
2023-06-15 15:40:51 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 pannellum.viewer('panorama-360-view', {
     "type": "equirectangular",
-    "panorama":"https://i.ibb.co/jDjTxk4/PANO-20230614-180837-0.jpg",
+    "panorama":"https://i.ibb.co/PQwQzpf/m1.jpg",
     "autoLoad": true,
     "autoPlay": true
 })
